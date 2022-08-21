5da48592cedc8295047509fe2d71016c51a9ec57
Emilson Quintero <e.quinterop@uniandes.edu.co>
2022-08-21 15:46:55 -0500

    Update NodeMCU_Mosquitto_SSL.ino

--- NodeMCU_Mosquitto_SSL/NodeMCU_Mosquitto_SSL.ino
+++ NodeMCU_Mosquitto_SSL/NodeMCU_Mosquitto_SSL.ino
@@ -8,29 +8,31 @@
 #define dht_dpin 4
 DHT dht(dht_dpin, DHTTYPE);
 
-#include "secrets.h"
+//#include "secrets.h"
 
 //Conexión a Wifi
 //Nombre de la red Wifi
-const char ssid[] = "wifi-name";
+const char ssid[] = "Guest";
 //Contraseña de la red Wifi
-const char pass[] = "wifi-password";
+const char pass[] = "BrokenWires@@2019";
 
 //Usuario uniandes sin @uniandes.edu.co
-#define HOSTNAME "nodeMCU-hostname"
+#define HOSTNAME "ln.bello10"
 
 //Conexión a Mosquitto
 const char MQTT_HOST[] = "iotlab.virtual.uniandes.edu.co";
 const int MQTT_PORT = 8082;
 //Usuario uniandes sin @uniandes.edu.co
-const char MQTT_USER[] = "mosquitto-user";
+const char MQTT_USER[] = "ln.bello10";
 //Contraseña de MQTT que recibió por correo
-const char MQTT_PASS[] = "mosquitto-password";
+const char MQTT_PASS[] = "201512763";
 const char MQTT_SUB_TOPIC[] = HOSTNAME "/";
 //Tópico al que se enviarán los datos de humedad
-const char MQTT_PUB_TOPIC1[] = "humedad/ciudad/" HOSTNAME;
+const char MQTT_PUB_TOPIC1[] = "humedad/londres/" HOSTNAME;
 //Tópico al que se enviarán los datos de temperatura
-const char MQTT_PUB_TOPIC2[] = "temperatura/ciudad/" HOSTNAME;
+const char MQTT_PUB_TOPIC2[] = "temperatura/londres/" HOSTNAME;
+//Tópico al que se enviarán los datos de luminosidad
+const char MQTT_PUB_TOPIC3[] = "luminosidad/londres/" HOSTNAME;
 
 //////////////////////////////////////////////////////
 
@@ -140,6 +142,7 @@ void setup()
   client.setCallback(receivedCallback);
   //Llama a la función de este programa que realiza la conexión con Mosquitto
   mqtt_connect();
+  dht.begin();
 }
 
 //Función loop que se ejecuta indefinidamente repitiendo el código a su interior
@@ -174,8 +177,10 @@ void loop()
   //Lee los datos del sensor
   float h = dht.readHumidity();
   float t = dht.readTemperature();
+  int l = analogRead(A0);
+  
   //Transforma la información a la notación JSON para poder enviar los datos 
-  //El mensaje que se envía es de la forma {"value": x}, donde x es el valor de temperatura o humedad
+  //El mensaje que se envía es de la forma {"value": x}, donde x es el valor de temperatura o humedad o intensidad luminica
   
   //JSON para humedad
   String json = "{\"value\": "+ String(h) + "}";
@@ -185,6 +190,10 @@ void loop()
   json = "{\"value\": "+ String(t) + "}";
   char payload2[json.length()+1];
   json.toCharArray(payload2,json.length()+1);
+  //JSON para luminosidad
+  json = "{\"value\": "+ String(l) + "}";
+  char payload3[json.length()+1];
+  json.toCharArray(payload3,json.length()+1);
 
   //Si los valores recolectados no son indefinidos, se envían a los tópicos correspondientes
   if ( !isnan(h) && !isnan(t) ) {
@@ -192,6 +201,8 @@ void loop()
     client.publish(MQTT_PUB_TOPIC1, payload1, false);
     //Publica en el tópico de la temperatura
     client.publish(MQTT_PUB_TOPIC2, payload2, false);
+    //Publica en el tópico de la luminosidad
+    client.publish(MQTT_PUB_TOPIC3, payload3, false);
   }
 
   //Imprime en el monitor serial la información recolectada
@@ -201,6 +212,10 @@ void loop()
   Serial.print(MQTT_PUB_TOPIC2);
   Serial.print(" -> ");
   Serial.println(payload2);
+  Serial.print(MQTT_PUB_TOPIC3);
+  Serial.print(" -> ");
+  Serial.println(payload3);
+  
   /*Espera 5 segundos antes de volver a ejecutar la función loop*/
   delay(5000);
 }
